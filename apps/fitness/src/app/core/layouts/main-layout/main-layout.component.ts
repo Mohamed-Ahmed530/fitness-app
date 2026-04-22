c8da64be4a2a7b7c942fad65b9016940c7bef2c1
Noura Arafa <nouraarafa180@gmail.com>
2026-04-22 19:32:51 +0200

    feat(home): add Footer section with scrolling marquee banner

--- apps/fitness/src/app/core/layouts/main-layout/main-layout.component.ts
+++ apps/fitness/src/app/core/layouts/main-layout/main-layout.component.ts
@@ -1,11 +1,11 @@
 import { Component } from '@angular/core';
-
 import { WhyUsComponent } from '../../../features/home/components/why-us/why-us.component';
+import { FooterComponent } from '../../../features/home/components/footer/footer.component';
 
 @Component({
   selector: 'app-main-layout',
   standalone: true,
-  imports: [WhyUsComponent],
+  imports: [WhyUsComponent, FooterComponent],
   templateUrl: './main-layout.component.html',
   styleUrl: './main-layout.component.scss',
 })
