62feb246cb361e1b9233ed1c55c5078efc774fab
Mohamed-Ahmed530 <mohamed.ahmed.mmaa.530@gmail.com>
2026-04-25 16:43:23 +0300

    create reusable UI components for auth system

--- apps/fitness/src/app/features/auth/components/register/register.component.ts
+++ apps/fitness/src/app/features/auth/components/register/register.component.ts
@@ -1,8 +1,12 @@
 import { Component } from '@angular/core';
+import { ButtonComponent } from "../../../../shared/components/ui/button/button.component";
+import { LabelComponent } from "../../../../shared/components/ui/label/label.component";
+import { InputComponent } from "../../../../shared/components/ui/input/input.component";
+import { ErrorMessageComponent } from "../../../../shared/components/ui/error-message/error-message.component";
 
 @Component({
   selector: 'app-register',
-  imports: [],
+  imports: [ButtonComponent, LabelComponent, InputComponent, ErrorMessageComponent],
   templateUrl: './register.component.html',
   styleUrl: './register.component.scss',
 })
---
+++ apps/fitness/src/app/shared/components/ui/button/button.component.ts
@@ -0,0 +1,18 @@
+import { NgClass } from '@angular/common';
+import { Component, input } from '@angular/core';
+import { FormGroup } from '@angular/forms';
+
+@Component({
+  selector: 'app-button',
+  imports: [NgClass],
+  templateUrl: './button.component.html',
+  styleUrl: './button.component.scss',
+})
+export class ButtonComponent {
+  label = input<string>('');
+  loading = input<boolean>(false);
+  disabled = input<boolean>(false);
+  parentForm = input<FormGroup>();
+  styleClass = input<string>('');
+  type = input<'button' | 'submit' | 'reset'>('button');
+}
