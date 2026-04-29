366afc54478bb14ab0f216191088812f77c57f5e
AsmaaMagdyy <asmaa.magdy8921@gmail.com>
2026-04-29 14:20:26 +0300

    saved changes

--- apps/fitness/src/app/features/auth/components/login/login.component.ts
+++ apps/fitness/src/app/features/auth/components/login/login.component.ts
@@ -1,9 +1,43 @@
-import { Component } from '@angular/core';
+import { Component, inject, signal, WritableSignal } from '@angular/core';
+import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import { Router, RouterLink } from '@angular/router';
+import { InputFieldComponent } from "apps/fitness/src/app/shared/ui/input-field/input-field.component";
+import { Subject } from 'rxjs';
 
 @Component({
   selector: 'app-login',
-  imports: [],
+  imports: [InputFieldComponent,ReactiveFormsModule,RouterLink],
   templateUrl: './login.component.html',
   styleUrl: './login.component.scss',
 })
-export class LoginComponent {}
+export class LoginComponent {
+  loginForm!:FormGroup;
+  // private readonly _authService = inject(AuthService);
+  private readonly _formBuilder = inject(FormBuilder);
+  private readonly _router = inject(Router);
+  private destroy$ = new Subject<void>();
+
+  togglePassword: WritableSignal<boolean> = signal(false);
+
+  ngOnInit(): void {
+    this.initForm();
+  }
+
+  initForm(): void {
+    this.loginForm = this._formBuilder.group({
+      email: [null, [Validators.required, Validators.email]],
+      password: [null, [Validators.required, Validators.pattern(/^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[#?!@$%^&*-]).{8,}$/)]]
+    })
+  }
+
+  submitForm():void{
+    if(this.loginForm.valid){
+      console.log(this.loginForm.value);
+    }
+  }
+
+   // toggle password
+  togglePasswordVisibility(): void {
+    this.togglePassword.update(prev => !prev);
+  }
+}
