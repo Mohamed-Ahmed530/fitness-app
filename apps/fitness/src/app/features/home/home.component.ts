0fbcf326823037bf02b022b363f08b02ad47beff
Mohamed-Ahmed530 <mohamed.ahmed.mmaa.530@gmail.com>
2026-04-30 20:41:22 +0300

    feature(home): implement hero section

--- apps/fitness/src/app/features/home/home.component.ts
+++ apps/fitness/src/app/features/home/home.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { NavigationBarComponent } from "../../core/components/navigation-bar/navigation-bar.component";
+import { HeroSectionComponent } from "./components/hero-section/hero-section.component";
 import { ServiceBarComponent } from "../../shared/ui/service-bar/serviceBar.component";
 import { AboutComponent } from "../about/components/about-component/about.component";
 
 @Component({
   selector: 'app-home',
-  imports: [NavigationBarComponent, ServiceBarComponent, AboutComponent],
+  imports: [NavigationBarComponent, HeroSectionComponent, ServiceBarComponent, AboutComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss',
 })
