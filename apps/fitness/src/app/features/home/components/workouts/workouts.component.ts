e9d9359a016399c166ab288752fa917671192946
AsmaaMagdyy <asmaa.magdy8921@gmail.com>
2026-06-23 00:19:54 +0300

    Implemented reusable tab

--- apps/fitness/src/app/features/home/components/workouts/workouts.component.ts
+++ apps/fitness/src/app/features/home/components/workouts/workouts.component.ts
@@ -2,6 +2,7 @@ import { Component, signal } from '@angular/core';
 import { TranslatePipe } from '@ngx-translate/core';
 import { CarouselComponent } from 'apps/fitness/src/app/shared/components/ui/carousel/carousel.component';
 import { SectionNameComponent } from "apps/fitness/src/app/shared/ui/section-name/sectionName.component";
+import { TabComponent } from "apps/fitness/src/app/shared/components/ui/tab/tab.component";
 
 // Define the Workout interface directly
 export interface Workout {
@@ -11,11 +12,16 @@ export interface Workout {
 }
 @Component({
   selector: 'app-workouts',
-  imports: [SectionNameComponent,CarouselComponent,TranslatePipe],
+  imports: [SectionNameComponent, CarouselComponent, TranslatePipe, TabComponent],
   templateUrl: './workouts.component.html',
   styleUrl: './workouts.component.scss',
 })
 export class WorkoutsComponent {
+
+  tabs:string[]=['full body','chest','arm','shoulder','back','legs','stomach'];
+
+  activeTab = signal<string>('full body');
+
   // Initialize inline mock data matching the interface
     workouts = signal<Workout[]>([
         {  title: 'Group workout', image: '/images/w1.png', link: '' },
@@ -25,5 +31,11 @@ export class WorkoutsComponent {
         {  title: 'personal training', image: '/images/w2.png', link: '' },
         {  title: 'muscle bilding', image: '/images/w3.png', link: '' }
     ]);
+
+    clickk(tabValue:string){
+      this.activeTab.set(tabValue);
+      console.log('Selected Tab:', tabValue);
+      
+    }
 
 }
